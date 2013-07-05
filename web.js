ead02d5b8d0832e00cd95691f1926481f9debe4e
Abhay.Mahajan <abhay.mahajan09@gmail.com>
2013-07-05 08:27:04 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -53,6 +53,10 @@ fs.exists(fileName, function(exists) {
   if (exists) {
     fs.stat(fileName, function(error, stats) {
       fs.open(fileName, "r", function(error, fd) {
+
+
+
+
         var buffer = new Buffer(stats.size);
         fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
           var data = buffer.toString("utf8", 0, buffer.length);
@@ -84,7 +88,7 @@ fs.exists(fileName, function(exists) {
   }
 });
 
-   response.send(data);
+//   response.send(data);
 });
 
 var port = process.env.PORT || 5000;
