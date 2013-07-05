5bb35643da3143685a4490e37a018263b072842a
Abhay.Mahajan <abhay.mahajan09@gmail.com>
2013-07-05 10:59:52 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -89,7 +89,7 @@ fs.exists(fileName, function(exists) {
 });
 
 
-var express = require('express');
+/*var express = require('express');
 var app = express.createServer(express.logger());
 var data =fs.readFileSync("index.html", "utf8");
 app.get('/', function(request, response) {
@@ -99,6 +99,6 @@ app.get('/', function(request, response) {
 
 //var port = process.env.PORT || 5000;
 //app.listen(port, function() {
-//  console.log("Listening on " + port);
+//  console.log("Listening on " + port);//
 //});
-
+*/
