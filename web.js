98e96454cac172ec2c8da383c390216221a9b137
Abhay.Mahajan <abhay.mahajan09@gmail.com>
2013-07-05 08:22:56 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -86,7 +86,8 @@ fs.exists(fileName, function(exists) {
 
    response.send(data);
 });
-//var port = process.env.PORT || 5000;
-//app.listen(port, function() {
-//  console.log("Listening on " + );
-//});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
