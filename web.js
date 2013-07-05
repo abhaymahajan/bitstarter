7dd03410e5310da4efd3cee36c949500e4262955
Abhay.Mahajan <abhay.mahajan09@gmail.com>
2013-07-05 12:43:52 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -28,8 +28,8 @@ app.get('/', function(request, response) {
   response.send(data);
 });
 
-//var port = process.env.PORT || 5000;
-//app.listen(port, function() {
-//  console.log("Listening on " + port);//
-//});
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);//
+});
 
